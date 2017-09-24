7a5f38bbce64f0fdbe63a1680bf9e77eb25bdd9c
KyuNam <xmfpes@naver.com>
2017-09-24 21:13:29 +0900

    글 수정 기능 구현

--- routes/admin.js
+++ routes/admin.js
@@ -17,7 +17,8 @@ router.get('/products', function(req, res){
 });
 
 router.get('/products/write', function(req,res){
-    res.render('admin/form');
+    //edit에서도 같은 form을 사용하므로 빈 변수( product )를 넣어서 에러를 피해준다
+    res.render( 'admin/form' , { product : "" }); 
 });
 
 router.post('/products/write', function(req,res){
@@ -35,6 +36,27 @@ router.get('/products/detail/:id' , function(req, res){
     //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
     ProductsModel.findOne( { 'id' :  req.params.id } , function(err ,product){
         res.render('admin/productsDetail', { product: product });  
+    });
+});
+
+router.get('/products/edit/:id' ,function(req, res){
+    //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
+    ProductsModel.findOne({ id : req.params.id } , function(err, product){
+        res.render('admin/form', { product : product });
+    });
+});
+
+router.post('/products/edit/:id', function(req, res){
+    //넣을 변수 값을 셋팅한다
+    var query = {
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    };
+ 
+    //update의 첫번째 인자는 조건, 두번째 인자는 바뀔 값들
+    ProductsModel.update({ id : req.params.id }, { $set : query }, function(err){
+        res.redirect('/admin/products/detail/' + req.params.id ); //수정후 본래보던 상세페이지로 이동
     });
 });
 
