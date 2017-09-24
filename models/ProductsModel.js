b37b1bc25e83523e6563c8e02a8731c870f08e7a
KyuNam <xmfpes@naver.com>
2017-09-24 21:04:58 +0900

    모델에 virtual attribute 추가

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var autoIncrement = require('mongoose-auto-increment');
+
 //생성될 필드명을 정한다.
 var ProductsSchema = new Schema({
     name : String, //제품명
@@ -11,6 +12,16 @@ var ProductsSchema = new Schema({
         default : Date.now()
     }
 });
+
+ProductsSchema.virtual('getDate').get(function(){
+    var date = new Date(this.created_at);
+    return {
+        year : date.getFullYear(),
+        month : date.getMonth()+1,
+        day : date.getDate()
+    };
+});
+
 // 1씩 증가하는 primary Key를 만든다
 // model : 생성할 document 이름
 // field : primary key , startAt : 1부터 시작
